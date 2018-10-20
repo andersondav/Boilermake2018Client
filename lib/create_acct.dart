09f492458bb70b9c3156f97e88bd48bbb33d7b6c
Russell Greene <russellgreene8@gmail.com>
2018-10-20 19:01:42 -0400

    Add geopicker

--- lib/create_acct.dart
+++ lib/create_acct.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_google_places_autocomplete/flutter_google_places_autocomplete.dart';
 
 import 'manage_acct.dart';
 
 final GoogleSignIn _googleSignIn = GoogleSignIn();
 final FirebaseAuth _auth = FirebaseAuth.instance;
+
+const MAPS_API_KEY = 'AIzaSyDx51vyR0IGRSrrtD9FVS6HVQOLWeRzGQ0';
 
 Future<FirebaseUser> _handleSignIn() async {
   GoogleSignInAccount googleUser = await _googleSignIn.signIn();
@@ -40,8 +43,12 @@ class _AcctCreationState extends State<AcctCreation> {
       'location': 'TODO',
     });
 
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => new ManageAccount(user: user)));
+    Navigator.pop(
+        context); // pop this off, if we go back we should go back to the main screen
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => new ManageAccount(user: user)),
+    );
   }
 
   // Create a text controller. We will use it to retrieve the current value
@@ -69,11 +76,9 @@ class _AcctCreationState extends State<AcctCreation> {
               //mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 new Text(
-                    'You are logged in as ${this.user.displayName}. Welcome new user!',
-                    style: new TextStyle(
-                      color: Colors.white
-                    ),
-                    textAlign: TextAlign.center,
+                  'You are logged in as ${this.user.displayName}. Welcome new user!',
+                  style: new TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
                 ),
                 new Container(
                   height: 20.0,
@@ -92,14 +97,14 @@ class _AcctCreationState extends State<AcctCreation> {
                 new TextFormField(
                   controller: myController,
                   decoration: new InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      contentPadding: new EdgeInsets.fromLTRB(
-                          10.0, 30.0, 10.0, 10.0),
-                      border: new OutlineInputBorder(
-                        borderRadius: new BorderRadius.circular(12.0),
-                      ),
-                      ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    contentPadding:
+                        new EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 10.0),
+                    border: new OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(12.0),
+                    ),
+                  ),
                 ),
                 new Container(
                   height: 20.0,
@@ -122,8 +127,8 @@ class _AcctCreationState extends State<AcctCreation> {
                   decoration: new InputDecoration(
                     fillColor: Colors.white,
                     filled: true,
-                    contentPadding: new EdgeInsets.fromLTRB(
-                        10.0, 30.0, 10.0, 10.0),
+                    contentPadding:
+                        new EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 10.0),
                     border: new OutlineInputBorder(
                       borderRadius: new BorderRadius.circular(12.0),
                     ),
@@ -133,21 +138,22 @@ class _AcctCreationState extends State<AcctCreation> {
                   height: 20.0,
                 ),
                 // ignore: list_element_type_not_assignable
+                new LocPickerButton(
+                  onResponse: (a) => print(a),
+                ),
                 ButtonTheme(
-                    height: 50.0,
-                    minWidth: 80.0,
-                    child: RaisedButton(
-                      splashColor: Colors.grey,
-                      color: Colors.blue,
-                      onPressed: _sendData,
-                      child: new Text(
-                          'Submit',
-                          style: new TextStyle(
-                            fontSize: 30.0,
-                            color: Colors.white,
-                          )
-                      ),
-                    ),
+                  height: 50.0,
+                  minWidth: 80.0,
+                  child: RaisedButton(
+                    splashColor: Colors.grey,
+                    color: Colors.blue,
+                    onPressed: _sendData,
+                    child: new Text('Submit',
+                        style: new TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.white,
+                        )),
+                  ),
                 ),
                 // ignore: argument_type_not_assignable
               ],
@@ -170,6 +176,8 @@ class _AcctCreationState extends State<AcctCreation> {
 
       if (document.exists) {
         // they arlready have an account, go to manage_acct
+        Navigator.pop(
+            context); // pop this off, if we go back we should go back to the main screen
         Navigator.push(
             context,
             MaterialPageRoute(
@@ -190,5 +198,36 @@ class _AcctCreationState extends State<AcctCreation> {
     } else {
       return _buildCreateAcct();
     }
+  }
+}
+
+class LocPickerButton extends StatefulWidget {
+  LocPickerButton({this.onResponse});
+
+  Function onResponse;
+
+  @override
+  _LocPickerButtonState createState() => _LocPickerButtonState();
+}
+
+class _LocPickerButtonState extends State<LocPickerButton> {
+  Prediction prediction;
+
+  void _getLoc(BuildContext context) async {
+    Prediction pre = await showGooglePlacesAutocomplete(
+        apiKey: MAPS_API_KEY, context: context);
+
+    setState(() => prediction = pre);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Row(children: [
+        Icon(Icons.map),
+        Text(prediction != null ? prediction.description : 'Pick a location')
+      ]),
+      onPressed: () => _getLoc(context),
+    );
   }
 }
