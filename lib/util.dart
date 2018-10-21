88049d9cee4b6524429d61be09cbd1a3eddf4b60
Russell Greene <russellgreene8@gmail.com>
2018-10-20 20:18:22 -0400

    Add distances calculations & views

---
+++ lib/util.dart
@@ -0,0 +1,28 @@
+import 'package:latlong/latlong.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+
+const MAPS_API_KEY = 'AIzaSyDx51vyR0IGRSrrtD9FVS6HVQOLWeRzGQ0';
+
+class LocationData {
+  LocationData({this.loc, this.name});
+
+  LatLng loc;
+  String name;
+}
+
+Future<LocationData> dataFromPlaceID(String placeID) async {
+  // get the lat+long of p
+  var response = await http.get(
+      'https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeID}&key=$MAPS_API_KEY');
+  var json = jsonDecode(response.body);
+
+  var result = json["result"];
+  var location = result["geometry"]["location"];
+  var lat = location["lat"];
+  var long = location["lng"];
+
+  return LocationData(
+      loc: LatLng(lat, long), name: result["formatted_address"]);
+}
